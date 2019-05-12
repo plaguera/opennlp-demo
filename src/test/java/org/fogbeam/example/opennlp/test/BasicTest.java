5937177ee48f05418ef9f51b54d54b1962477c6c
Pedro Lagüera <laguerapedro@gmail.com>
2019-05-12 11:59:38 +0100

    Added Basic Tests

---
+++ src/test/java/org/fogbeam/example/opennlp/test/BasicTest.java
@@ -0,0 +1,21 @@
+package org.fogbeam.example.opennlp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BasicTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+		
+	}
+
+}
