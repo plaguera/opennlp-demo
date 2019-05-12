4c8c707e9ad95cd5d0326dd8c83737c55c816d75
Pedro Lagüera <laguerapedro@gmail.com>
2019-05-12 13:56:59 +0100

    New Tokenizer Tests

--- src/test/java/org/fogbeam/example/opennlp/test/BasicTest.java
+++ src/test/java/org/fogbeam/example/opennlp/test/BasicTest.java
@@ -25,11 +25,40 @@ public class BasicTest {
 	}
 
 	@Test
-	public void test() throws InvalidFormatException, IOException {
+	public void test1() throws InvalidFormatException, IOException {
 		String[] tokens = tokenizer.tokenize("A ranger journeying");
+		assertEquals(3, tokens.length);
 		assertEquals("A", tokens[0]);
 		assertEquals("ranger", tokens[1]);
 		assertEquals("journeying", tokens[2]);
+	}
+	
+	@Test
+	public void test2() throws InvalidFormatException, IOException {
+		String[] tokens = tokenizer.tokenize("Great Kings who were killed in the Wars.");
+		assertEquals(9, tokens.length);
+		assertEquals("Great", tokens[0]);
+		assertEquals("Kings", tokens[1]);
+		assertEquals("who", tokens[2]);
+		assertEquals("were", tokens[3]);
+		assertEquals("killed", tokens[4]);
+		assertEquals("in", tokens[5]);
+		assertEquals("the", tokens[6]);
+		assertEquals("Wars", tokens[7]);
+		assertEquals(".", tokens[8]);
+	}
+	
+	@Test
+	public void test3() throws InvalidFormatException, IOException {
+		String[] tokens = tokenizer.tokenize("Oglethorpe, founder of the Georgia Colony");
+		assertEquals(7, tokens.length);
+		assertEquals("Oglethorpe", tokens[0]);
+		assertEquals(",", tokens[1]);
+		assertEquals("founder", tokens[2]);
+		assertEquals("of", tokens[3]);
+		assertEquals("the", tokens[4]);
+		assertEquals("Georgia", tokens[5]);
+		assertEquals("Colony", tokens[6]);
 	}
 
 }
